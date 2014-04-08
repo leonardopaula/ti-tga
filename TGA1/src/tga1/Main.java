3d5615a02377fc67c45b65d921b13fa2dc3258a3
Leonardo S. Paula <leonardopaula@gmail.com>
2014-04-08 20:46:55 -0300

    Lendo arquivo

--- TGA1/src/tga1/Main.java
+++ TGA1/src/tga1/Main.java
@@ -6,6 +6,11 @@
 
 package tga1;
 
+import java.io.*;
+import java.nio.charset.Charset;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author leonardo
@@ -15,8 +20,21 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        File file = new File("/home/leonardo/NetBeansProjects/ti-tga/TGA1/src/test/opa.txt");
+        try {
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(
+                            new FileInputStream(file),
+                            Charset.forName("UTF-8")));
+            
+            int c = 0;
+            while((c = reader.read()) != -1) {
+                    char character = (char) c;
+                    System.out.println(Integer.toBinaryString(character));
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
-    
 }
